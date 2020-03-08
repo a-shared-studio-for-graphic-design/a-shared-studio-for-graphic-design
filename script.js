d15a4040ff9b7ea9590f4723e1f5809a7c8283f1
Kevin Feng <kjfeng@princeton.edu>
2020-03-08 17:52:54 +0800

    http --> https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('.work-img').resizable();
   $('.block').draggable({ containment: ".kv-wrapper", scroll: false });
-  $('.timestamp-text').html("last modified " + document.lastModified);
+  $('.timestamp-text').html("last addition " + document.lastModified);
   // $("body").on('DOMSubtreeModified', ".kv-wrapper", function() {
   //   alert('changed');
   // });
