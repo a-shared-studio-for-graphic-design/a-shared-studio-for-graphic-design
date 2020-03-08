9fbcdef3413328b4f3662e0ecf87072f5b9a7c1c
Kevin Feng <kjfeng@princeton.edu>
2020-03-08 17:34:09 +0800

    restrict draggable movement

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
   $('.work-img').resizable();
-  $('.block').draggable();
+  $('.block').draggable({ containment: ".kv-wrapper", scroll: false });
+  $('.timestamp-text').html("last modified " + document.lastModified);
+  // $("body").on('DOMSubtreeModified', ".kv-wrapper", function() {
+  //   alert('changed');
+  // });
 });
